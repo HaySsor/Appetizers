7b36eb8394cddc39afd530f7cc5d589ab10f0a53
Przemek Hussar <hayss007@gmail.com>
2023-11-05 12:29:39 +0100

    Initial Commit

---
+++ Appetizers1/Appetizers1App.swift
@@ -0,0 +1,17 @@
+//
+//  Appetizers1App.swift
+//  Appetizers1
+//
+//  Created by Przemek Hussar on 05/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Appetizers1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
